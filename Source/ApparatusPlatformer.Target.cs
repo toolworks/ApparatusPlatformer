1a0b94df4cc823409d796d0e2183fb910358a5c0
Vladislav Dmitrievich Turbanov <vladislav@turbanov.ru>
2021-04-05 12:34:32 +0300

    * Apparatus platformer main target with root sources.

---
+++ Source/ApparatusPlatformer.Target.cs
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ApparatusPlatformerTarget : TargetRules
+{
+    public ApparatusPlatformerTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+
+        ExtraModuleNames.AddRange(new string[] { "ApparatusRuntime" });
+    }
+}
